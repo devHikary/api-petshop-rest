0325ff48e202308aed24be9c6dec0646474d3824
devHikary <wrkkarina@gmail.com>
2021-08-07 20:25:05 -0300

    Aula 2.1

--- api/rotas/fornecedores/index.js
+++ api/rotas/fornecedores/index.js
@@ -1,11 +1,15 @@
 const roteador = require('express').Router()
 const TabelaFornecedor = require('./TabelaFornecedor')
 
-roteador.use('/', async (req, res) => {
+roteador.get('/', async (req, res) => {
   const resultados = await TabelaFornecedor.listar()
   res.send(
     resultados
   )
+})
+
+roteador.post('/', (req, res) => {
+
 })
 
 module.exports = roteador
